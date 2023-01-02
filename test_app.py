0cf24f11bcab288789ad6cb343a6bbdcf622d2bc
zaky yusuf <59588009+zakyyusuff@users.noreply.github.com>
2023-01-02 12:30:49 +0700

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,20 @@
+from fungsi import *
+import pytest
+
+class TestApp:
+    def test_show_type_text(self):
+        print('text:')
+        show_type_of_text("Go until jurong point, crazy..")
+        assert True
+    def test_show_type_of_text(self):
+        print('text:')
+        show_type_of_text("FreeMsg Hey there darling it's been 3 week's now and no word back!", False)
+        assert True
+    def test_type(self):
+        print('Take a name: (give text and first word)')   
+        give_type()
+        assert True
+
+    if __name__ == '__main__':
+        print("run with 'pytest -v test_fungsi.py'") #jalan
+        pytest.main
